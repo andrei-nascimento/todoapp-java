3f2dc985af8841b459d27bb060d7758e2020257e
Andrei Nascimento <andrei.lima.nas@outlook.com>
2023-03-22 14:43:35 -0300

    Criação da lista de projetos na model

--- app/src/main/java/view/MainScreen.java
+++ app/src/main/java/view/MainScreen.java
@@ -4,8 +4,15 @@
  */
 package view;
 
+import controller.ProjectController;
+import controller.TaskController;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.List;
+import javax.swing.DefaultListModel;
+import model.Project;
 
 /**
  *
@@ -13,12 +20,17 @@ import java.awt.Font;
  */
 public class MainScreen extends javax.swing.JFrame {
 
-    /**
-     * Creates new form MainScreen
-     */
+    ProjectController projectController;
+    TaskController taskController;
+    
+    DefaultListModel projectModel;
+    
     public MainScreen() {
         initComponents();
         decorateTableTasks();
+        
+        initDataController();
+        initComponentsModel();
     }
 
     /**
@@ -137,7 +149,7 @@ public class MainScreen extends javax.swing.JFrame {
             .addGroup(ProjectsLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(ProjectsTitle, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 38, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(ProjectsAdd)
                 .addContainerGap())
         );
@@ -194,11 +206,6 @@ public class MainScreen extends javax.swing.JFrame {
         ProjectItems.setBackground(new java.awt.Color(255, 255, 255));
         ProjectItems.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         ProjectItems.setForeground(new java.awt.Color(0, 0, 0));
-        ProjectItems.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
         ProjectItems.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
         ProjectItems.setFixedCellHeight(50);
         ProjectItems.setSelectionBackground(new java.awt.Color(0, 153, 102));
@@ -307,6 +314,12 @@ public class MainScreen extends javax.swing.JFrame {
         // TODO add your handling code here:
         ProjectDialogScreen projectDialogScreen = new ProjectDialogScreen(this, rootPaneCheckingEnabled);
         projectDialogScreen.setVisible(true);
+        
+        projectDialogScreen.addWindowListener(new WindowAdapter() {
+            public void windowClosed(WindowEvent e) {
+                loadProjects();
+            }
+        }); 
     }//GEN-LAST:event_ProjectsAddMouseClicked
 
     private void TasksAddMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_TasksAddMouseClicked
@@ -381,5 +394,26 @@ public class MainScreen extends javax.swing.JFrame {
         
         //Criando um sort automático para as colunas da table
         TableTasks.setAutoCreateRowSorter(true);
+    }
+    
+    public void initDataController() {
+        projectController = new ProjectController();
+        taskController = new TaskController();
+    }
+    
+    public void initComponentsModel() {
+        projectModel = new DefaultListModel();
+        loadProjects();
+    }
+    
+    public void loadProjects() {
+        List<Project> projects = projectController.getAll();
+        projectModel.clear();
+        
+        for (int i = 0; i < projects.size(); i++) {
+            Project project = projects.get(i);
+            projectModel.addElement(project);
+        }
+        ProjectItems.setModel(projectModel);
     }
 }
